c686e4f1934f1963e81b1bdd7877f433029ca77d
gsp777 <prakash.g@teksacademy.com>
2025-06-26 08:11:30 +0530

    Completed Chatbot

--- src/chatbot/Chatbot.jsx
+++ src/chatbot/Chatbot.jsx
@@ -66,34 +66,128 @@ import {
 import { Input } from "@/components/ui/input";
 
 const Chatbot = () => {
-    return (
-        <Dialog>
-            <form>
-                <DialogTrigger asChild>
-                    <div className="div">
-                           <img src="https://cdn-icons-png.flaticon.com/128/3649/3649460.png" className='w-18 border-3 border-black rounded-full p-1 bg-purple-300 cursor-pointer fixed bottom-5 right-5 z-99' alt="" />
-                    </div>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px] bg-white">
-                    <DialogHeader>
-                        <DialogTitle>ChatBot</DialogTitle>
-                        <DialogDescription>
-                            Ask your questions! I am there to help you out.
-                        </DialogDescription>
-                    </DialogHeader>
-                  <div className="min-h-40 bg-gray-200">
-                      <blockquote className="mt-6 border-l-2 pl-6 italic"> &quot;Hey User,&quot;  &quot;Here you'll get chat conversation.&quot; </blockquote>
+
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const [generatingAnswer, setGeneratingAnswer] = useState(false);
+
+
+
+  console.log(chatHistory, "chatHistory")
+
+
+  const chatContainerRef = useRef(null);
+
+
+   useEffect(()=>{
+    if(chatContainerRef.current){
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+   },[chatHistory, generateAnswer])
+
+  console.log(chatContainerRef,"chatContainerReference")
+  async function generateAnswer(e) {
+    e.preventDefault();
+
+    if (!question.trim()) return;
+
+    setGeneratingAnswer(true);
+
+    const currentQuestion = question;
+
+   
+
+    setChatHistory((prev) => [...prev, { type: "question", content: currentQuestion }]);
+
+    try {
+      const response = await axios(
+        {
+          url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAX4twBRL8Tr65fsTQjr8Dq3gL-Zs2Hxy4",
+          method: "post",
+          data: {
+            contents: [{ parts: [{ text: currentQuestion }] }]
+          }
+        });
+
+      const aiResponse = response.data.candidates[0].content.parts[0].text;
+      setChatHistory((prev) => [...prev, { type: "answer", content: aiResponse }])
+      setAnswer(aiResponse);
+    }
+    catch (error) {
+      console.log(error);
+      setAnswer("Sorry! - Something went wromg, Try Again!")
+    }
+    setGeneratingAnswer(false)
+     setQuestion("")
+  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="div">
+          <img src="https://cdn-icons-png.flaticon.com/128/3649/3649460.png" className='w-18 border-3 border-black rounded-full p-1 bg-purple-300 cursor-pointer fixed bottom-5 right-5 z-99' alt="" />
+        </div>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] bg-white">
+        <DialogHeader>
+          <DialogTitle>ChatBot</DialogTitle>
+          <DialogDescription>
+            Ask your questions! I am there to help you out.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex-1 max-h-[400px] overflow-y-auto px-4 py-2" ref={chatContainerRef}>
+        {
+          chatHistory.length === 0 ? (
+            <div>
+              Ask me Something!, I'll give you the best response.
+            </div>
+          ):(
+            chatHistory.map((chat,index)=>(
+              <div key={index}
+              className={`my-2 ${chat.type === "question" ? "text-right" : "text-left"}`}
+              >
+                <div className={`inline-block max-w-[80%] p-3 rounded text-sm ${
+                  chat.type === 'question' ? "bg-red-400 text-white rounded-br-none"
+                  : "bg-gray-200 text-gray-800 rounded-bl-none"
+                }`}>
+                  <ReactMarkdown>{chat.content}</ReactMarkdown>
+                </div>
+              </div>
+            ))
+          )}
+          {
+            generatingAnswer && (
+              <div>
+                <div className="text-left mt-2">
+                  <div className="inline-block bg-gray-100 p-3 rounded animate-pulse">
+                    Generating....
                   </div>
-                    <DialogFooter>
-                        <div className="grid grid-cols-5 gap-2">
-                            <Input id="name-1" name="name" className="col-span-4" placeholder="Ask your thoughts!" />
-                            <Button variant="outline" className="cursor-pointer col-span-1">Ask</Button>
-                        </div>
-                    </DialogFooter>
-                </DialogContent>
-            </form>
-        </Dialog>
-    )
+                </div>
+              </div>
+            )
+          }
+        </div>
+        <DialogFooter>
+          <form onSubmit={generateAnswer}>
+            <div className="flex gap-3">
+              <textarea name="question" id="" placeholder="Ask Anything!" onChange={(event) => setQuestion(event.target.value)}
+
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault()
+                    generateAnswer(e)
+                  }
+                }}
+
+              ></textarea>
+              <Button type="submit" className={`px-4 py-2 text-md ${generatingAnswer ? "opacity-50 cursor-not-allowed" : ""}`} >Ask!</Button>
+            </div>
+          </form>
+        </DialogFooter>
+      </DialogContent>
+
+    </Dialog>
+  )
 };
 
 export default Chatbot;
--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -28,61 +28,60 @@ const Navbar = () => {
     <nav className="p-1 bg-white shadow-sm">
       <div className="container mx-auto flex justify-between items-center px-4 flex-wrap">
         <NavLink to="/">
-          <img 
-            src="https://eternatek.in/wp-content/uploads/2024/08/WhatsApp_Image_2024-08-31_at_11.46.53_AM-removebg-preview.png" 
-            alt="Logo" 
-            className='w-[160px] h-[50px]' 
+          <img
+            src="https://eternatek.in/wp-content/uploads/2024/08/WhatsApp_Image_2024-08-31_at_11.46.53_AM-removebg-preview.png"
+            alt="Logo"
+            className='w-[160px] h-[50px]'
           />
         </NavLink>
         <h1 className='hidden max-[1020px]:block'>Icon</h1>
         <div className="flex w-auto gap-12 items-center text-base font-medium justify-center max-[1020px]:hidden">
           <NavLink to="/" className={({ isActive }) =>
-           isActive
-          ? "bg-gradient-to-r from-purple-600 to-black text-white px-3 py-1 rounded-lg font-semibold shadow-lg transform scale-105 border border-purple-500 transition-all duration-ease-in-out"
-          : "hover:text-purple-800 hover:bg-purple-100 px-3 py-1 rounded transition-colors text-gray-700"
+            isActive
+              ? "bg-gradient-to-r from-purple-600 to-black text-white px-3 py-1 rounded-lg font-semibold shadow-lg transform scale-105 border border-purple-500 transition-all duration-ease-in-out"
+              : "hover:text-purple-800 hover:bg-purple-100 px-3 py-1 rounded transition-colors text-gray-700"
           }>
-          Home
-         </NavLink>
-          
+            Home
+          </NavLink>
+
           <NavLink to="/about" className={({ isActive }) =>
-          isActive
-          ? "bg-gradient-to-r from-purple-600 to-black text-white px-3 py-1 rounded-lg font-semibold shadow-lg transform scale-105 border border-purple-500 transition-all duration-ease-in-out"
-          : "hover:text-purple-800 hover:bg-purple-100 px-3 py-1 rounded transition-colors text-gray-700"
+            isActive
+              ? "bg-gradient-to-r from-purple-600 to-black text-white px-3 py-1 rounded-lg font-semibold shadow-lg transform scale-105 border border-purple-500 transition-all duration-ease-in-out"
+              : "hover:text-purple-800 hover:bg-purple-100 px-3 py-1 rounded transition-colors text-gray-700"
           }>
             About
           </NavLink>
 
           <NavLink to="/contact" className={({ isActive }) =>
-          isActive
-          ? " bg-gradient-to-r from-purple-600 to-black text-white px-3 py-1 rounded-lg font-semibold shadow-lg transform scale-105 border border-purple-500 transition-all duration-ease-in-out"
-          : "hover:text-purple-800 hover:bg-purple-100 px-3 py-1 rounded transition-colors text-gray-700"
+            isActive
+              ? " bg-gradient-to-r from-purple-600 to-black text-white px-3 py-1 rounded-lg font-semibold shadow-lg transform scale-105 border border-purple-500 transition-all duration-ease-in-out"
+              : "hover:text-purple-800 hover:bg-purple-100 px-3 py-1 rounded transition-colors text-gray-700"
           }>
-          Contact
+            Contact
           </NavLink>
 
           <NavigationMenu className="w-auto">
             <NavigationMenuList >
               <NavigationMenuItem >
                 <NavigationMenuTrigger className={({ isActive }) =>
-          isActive
-          ? "bg-gradient-to-r from-purple-600 to-black text-white rounded-lg font-semibold shadow-lg transform scale-105 border border-purple-500 transition-all duration-ease-in-out"
-          : "hover:text-purple-800 hover:bg-purple-100 px-3 py-1 rounded transition-colors text-gray-700"
-          }>
-          Services
+                  isActive
+                    ? "bg-gradient-to-r from-purple-600 to-black text-white rounded-lg font-semibold shadow-lg transform scale-105 border border-purple-500 transition-all duration-ease-in-out"
+                    : "hover:text-purple-800 hover:bg-purple-100 px-3 py-1 rounded transition-colors text-gray-700"
+                }>
+                  Services
                 </NavigationMenuTrigger>
-                <NavigationMenuContent className="bg-black border-t border-gray-700 shadow-lg rounded-lg">
-                  <div className="p-2 w-full gap-2">
+                <NavigationMenuContent className="bg-black shadow-lg ">
+                  <div className="p-2 w-[200px] gap-2">
                     {serviceLinks.map((service) => (
                       <NavLink
                         key={service.to}
                         to={service.to}
-                        className={({ isActive }) => 
-                      `flex items-center justify-center px-4 py-2 rounded-md transition-colors duration-200 ${
-            isActive
-              ? 'bg-white text-black font-semibold'
-              : 'bg-black text-purple-600 hover:bg-white hover:text-black'
+                        className={({ isActive }) =>
+                          `flex items-center justify-center px-4 py-2 rounded-lg transition-colors duration-200 ${isActive
+                            ? 'bg-white text-black font-semibold'
+                            : 'bg-black text-purple-600 hover:bg-white hover:text-black'
 
-                        }`
+                          }`
                         }
                       >
                         {service.label}
@@ -93,17 +92,17 @@ const Navbar = () => {
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
-          
-         <NavLink 
-         to="/login" 
-         className={({ isActive }) =>
-         isActive
-         ? "bg-gradient-to-r from-purple-600 to-purple-800 hover:from-purple-700 hover:to-purple-900 text-white font-semibold py-1 px-3 rounded-lg shadow-lg transform scale-105 border-2 border-purple-500"
-         : "text-purple-700 hover:text-white hover:bg-gradient-to-r hover:from-purple-600 hover:to-purple-800 px-3 py-1 rounded-lg transition-all duration-300 ease-in-out border-2 border-purple-600 bg-white shadow-md hover:shadow-lg hover:scale-105"
-       }
-       >
-         LogIn
-        </NavLink>
+
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-gradient-to-r from-purple-600 to-purple-800 hover:from-purple-700 hover:to-purple-900 text-white font-semibold py-1 px-3 rounded-lg shadow-lg transform scale-105 border-2 border-purple-500"
+                : "text-purple-700 hover:text-white hover:bg-gradient-to-r hover:from-purple-600 hover:to-purple-800 px-3 py-1 rounded-lg transition-all duration-300 ease-in-out border-2 border-purple-600 bg-white shadow-md hover:shadow-lg hover:scale-105"
+            }
+          >
+            LogIn
+          </NavLink>
         </div>
       </div>
     </nav>
