b8616f3a2be272b04a05c3a6fb67d9826d7066f8
gsp777 <prakash.g@teksacademy.com>
2025-07-01 08:13:36 +0530

    Task : Sticky navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,13 +5,16 @@ import Footer from "../utils/Footer";
 import SimpleSlider from "../utils/SimpleSlider";
 import HeaderCarousel from "../utils/HeaderCarousel";
 import Chatbot from "../chatbot/Chatbot";
+import Headroom from "react-headroom";
 
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      <Headroom>
+        <Navbar />
+      </Headroom>
       <hr className="mb-2" />
-      <Chatbot/>
+      <Chatbot />
       {/* <SimpleSlider/> */}
       {/* <HeaderCarousel/> */}
       <main className="flex-grow p-4">
